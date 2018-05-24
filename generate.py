d1addadd554c85c2f4ec327652ff12acfc81c444
karthik <reachkarthikmohan@gmail.com>
2018-05-24 14:30:23 +0530

    sudoku class definition

--- generate.py
+++ generate.py
@@ -1,23 +1,30 @@
 import numpy as np
 import random as rd
-
-size = 9
-cell = np.arange(1,size+1)
-rd.shuffle(cell)
-cells = np.array([cell])
-for x in range(9):
-    rd.shuffle(cell)
-    cells = np.vstack([cells,cell])
-    pass
-print(cells)
-i=0
-while( i <= 6 ):
-    j=0
-    while( j <= 6 ): 
-        # print(cells[i][j]," ",cells[i][j+1])
-        print(cells[i][j]," ",cells[i][j+1]," ",cells[i][j+2]," | ",cells[i+1][j]," ",cells[i+1][j+1]," ",cells[i+1][j+2]," | ",cells[i+2][j]," ",cells[i+2][j+1]," ",cells[i+2][j+2])
-        j+=3
+class Sudoku:
+    def __init__(self):
+        self.size = 9
+        self.cell = np.arange(1,self.size+1)
+        rd.shuffle(self.cell)
+        self.cells = np.array([self.cell])
+        for x in range(9):
+            rd.shuffle(self.cell)
+            self.cells = np.vstack([self.cells,self.cell])
+            pass
+        print(self.cells)
         pass
-    print("-------------------------------------")    
-    i+=3
-    pass        
+    # sudoku sorted in self.cells
+    def printSudoku(self):
+        i=0
+        while( i <= 6 ):
+            j=0
+            while( j <= 6 ):
+                # print(self.cells[i][j]," ",self.cells[i][j+1])
+                print(self.cells[i][j]," ",self.cells[i][j+1]," ",self.cells[i][j+2]," | ",self.cells[i+1][j]," ",self.cells[i+1][j+1]," ",self.cells[i+1][j+2]," | ",self.cells[i+2][j]," ",self.cells[i+2][j+1]," ",self.cells[i+2][j+2])
+                j+=3
+                pass
+            print("-------------------------------------")
+            i+=3
+            pass
+        pass
+# lassie = Sudoku();
+# lassie.printSudoku()
